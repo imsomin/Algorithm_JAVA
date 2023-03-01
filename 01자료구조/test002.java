021f5094acb332e7f638a36f2d808348f77d74d5
imsomin <ssuna0130@gmail.com>
2023-03-01 22:49:55 +0900

    자료구조_배열_2

--- 01자료구조/test002.java
+++ 01자료구조/test002.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 
+//002. 평균 구하기
+/*
+모든 점수/받은 점수 중 최대점수 * 100
+위와 같이 점수를 조작하고
+조작한 점수로 받은 새로운 평균을 구하는 프로그램 작성
+*/
+
 public class test002 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int A[] = new int[N];
-        for(int i=0; i<N; i++){
+        int N = sc.nextInt();  //(1)시험 본 과목의 개수 N 선언
+        int A[] = new int[N];  //(2)각 과목의 시험 성적을 담을 배열 선언
+
+        for(int i=0; i<N; i++){ //(3)A[] 길이만큼 반복 및 A[i]에 각 점수 저장하기
             A[i] = sc.nextInt();
         }
-        long sum = 0; 
+        long sum = 0; //(4)점수(총점,최대점수) 초기화
         long max = 0;
         for (int i=0; i<N; i++){
-            if (A[i] > max) max = A[i];
+            if (A[i] > max) 
+            max = A[i];
             sum = sum + A[i];
         }
 
         System.out.println(sum * 100.0 / max / N);
+        //(5)변환 점수의 평균을 구하는 식을 간단히 표현 가능
+        // (A/M*100 + B/M*100 + C/M*100) / 3
+        //위의 식은 아래의 식과 같음
+        // (A+B+C) * 100 / M / 3
+        
 
 
         sc.close();
--- 01자료구조/test003.java
+++ 01자료구조/test003.java
@@ -1,6 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//003. 구간 합 구하기
+//수 N개가 주어졌을 때 i 번째 수에서 j 번째 수까지의 합을 구하는 프로그램 작성
+
 public class test003 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[suNo + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for (int q = 0; q < quizNo; q++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i-1]);
+        } 
     }
     
 }

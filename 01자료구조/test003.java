a5a0386eccf71d426f890a7508dba603c4c8b59c
imsomin <ssuna0130@gmail.com>
2023-03-03 22:48:31 +0900

    자료구조_구간합

--- 01자료구조/test003.java
+++ 01자료구조/test003.java
@@ -10,21 +10,25 @@ public class test003 {
     public static void main(String[] args) throws IOException{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        //StringTokenizer 클래스
+        //하나의 문자열을 여러 개의 문자열로 분리하기 위해 사용된다
         
-        int suNo = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
-        long[] S = new long[suNo + 1];
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());  //숫자 개수 ex.5
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());  //질의 개수 ex.3
+
+        long[] S = new long[suNo + 1];  //합 배열 S ex.5 4 3 2 1
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 1; i <= suNo; i++) {
-            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+
+        for (int i = 1; i <= suNo; i++) {  //숫자 개수만큼 반복하기  
+            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());  //합 배열 생성
         }
 
-        for (int q = 0; q < quizNo; q++){
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int q = 0; q < quizNo; q++){  //질의 개수만큼 반복하기
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());  //질의 범위 받기(i~j)
             int i = Integer.parseInt(stringTokenizer.nextToken());
             int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j] - S[i-1]);
-        } 
+            System.out.println(S[j] - S[i-1]);  //구간 합 출력하기
+        }
     }
     
 }
